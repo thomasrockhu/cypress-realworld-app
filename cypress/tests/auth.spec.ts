db41fa40fd0e6c798b5a4999f7d9311f30f28951
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-12-29 18:27:01 -0500

    add wrong user test

--- cypress/tests/auth.spec.ts
+++ cypress/tests/auth.spec.ts
@@ -1,7 +1,18 @@
 describe("Auth", () => {
-  it("redirects visitors to sign in page", () => {
+  beforeEach(() => {
     cy.task("db:seed");
+  });
+
+  it("redirects visitors to sign in page", () => {
     cy.visit("/personal");
     cy.location("pathname").should("equal", "/signin");
+  });
+
+  it("shows an error for invalid user", () => {
+    cy.visit("/signin");
+    cy.get("#username").type("Unknown user");
+    cy.get("#password").type("wrongPassword");
+    cy.contains("button", "Sign In").click();
+    cy.contains("[data-test=signin-error]", "Username or password is invalid").should("be.visible");
   });
 });
